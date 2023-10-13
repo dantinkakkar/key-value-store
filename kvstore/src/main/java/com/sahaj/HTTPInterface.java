d23cd528fe35391f3ed8f4c92eda454826781d41
dantinkakkar <dantin.kakkar@gmail.com>
2023-10-13 13:57:22 +0530

    Use multiple buffers

--- kvstore/src/main/java/com/sahaj/HTTPInterface.java
+++ kvstore/src/main/java/com/sahaj/HTTPInterface.java
@@ -3,34 +3,42 @@ package com.sahaj;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import javafx.util.Pair;
 
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 
 public class HTTPInterface {
 
     public static void main(String[] args) throws Exception {
         final long durabilityInMs = Long.parseLong(args[0]);
+        final short storesToUse = Short.parseShort(args[1]);
         final long bufferSize = (durabilityInMs/2)*3140000 < 0 ? Integer.MAX_VALUE - 8 : (durabilityInMs/2)*3140000;
-        final File storeFile = new File("store");
-        if (!storeFile.exists()) {
-            storeFile.createNewFile();
+        final Map<String, Pair<Long, String>> underlyingMap = new ConcurrentHashMap<>();
+        final List<File> stores = new ArrayList<>();
+        final List<BufferedWriter> writers = new ArrayList<>();
+        for (short i=0;i<storesToUse;i++) {
+            final File file = new File("store" + i);
+            if (!file.exists()) file.createNewFile();
+            stores.add(file);
+            writers.add(new BufferedWriter(new FileWriter(file, true),((Long) bufferSize).intValue()/storesToUse));
         }
-        final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(storeFile, true),
-                ((Long) bufferSize).intValue());
         final ScheduledExecutorService flushService = Executors.newSingleThreadScheduledExecutor();
         flushService.scheduleAtFixedRate(new Thread(() -> {
             try {
-                bufferedWriter.flush();
+                for(BufferedWriter w : writers) w.flush();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }), 0, durabilityInMs/2, TimeUnit.MILLISECONDS);
-        final KeyValueStore keyValueStore = new KeyValueStore(new HashMap<>(), bufferedWriter);
-        Undertow server = Undertow
+        final KeyValueStore keyValueStore = new KeyValueStore(underlyingMap, writers);
+        final Undertow server = Undertow
                 .builder()
                 .addHttpListener(8000, "localhost")
                 .setHandler(new StoreHandler(keyValueStore))
@@ -38,7 +46,7 @@ public class HTTPInterface {
         server.start();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
-                bufferedWriter.close();
+                for (BufferedWriter w : writers) w.close();
                 flushService.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -64,8 +72,9 @@ public class HTTPInterface {
             }
         }
 
-        private void handlePut(final HttpServerExchange t, final String key, final String value) throws IOException {
-            store.writeValue(key, value);
+        private void handlePut(final HttpServerExchange t, final String key, final String value) {
+            final long currentTime = System.nanoTime();
+            store.writeValue(key, value, currentTime);
             t.getResponseSender().close();
         }
 
@@ -78,5 +87,18 @@ public class HTTPInterface {
             t.setStatusCode(405);
             t.getResponseSender().close();
         }
+    }
+
+    private static void initializeStores(final File storeFile, final Map<String, Pair<Long, String>> initStore) throws IOException {
+        Files.readAllLines(Path.of(storeFile.toURI())).forEach(line -> {
+            final String[] parts = line.split(":");
+            final String key = parts[0];
+            final String newValue = parts[1];
+            final long newTime = Long.parseLong(parts[2]);
+            initStore.compute(key, (k, oldPair) -> {
+                final long oldTime = oldPair != null ? oldPair.getKey() : 0;
+                return newTime > oldTime ? new Pair<>(newTime, newValue) : oldPair;
+            });
+        });
     }
 }
--- kvstore/src/main/java/com/sahaj/KeyValueStore.java
+++ kvstore/src/main/java/com/sahaj/KeyValueStore.java
@@ -3,33 +3,37 @@ package com.sahaj;
 import javafx.util.Pair;
 
 import java.io.*;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class KeyValueStore {
 
     private final Map<String, Pair<Long, String>> underlyingStore;
-    private final BufferedWriter writer;
+    private final List<BufferedWriter> writers;
     public KeyValueStore(
             final Map<String, Pair<Long, String>> underlyingStoreImpl,
-            final BufferedWriter writerImpl
-    ) {
+            final List<BufferedWriter> writers
+            ) {
         this.underlyingStore = underlyingStoreImpl;
-        this.writer = writerImpl;
+        this.writers = writers;
     }
 
-    public void writeValue(String key, String value) {
-        underlyingStore.compute(key, (__, oldValue) -> {
-            long time = System.nanoTime();
-            final long oldTime = oldValue != null ? oldValue.getKey() : 0;
-            try {
-                writer.write(key+":"+value+":"+time);
-                writer.newLine();
-                return time > oldTime ? new Pair<>(time, value) : oldValue;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return oldValue;
-            }
-        });
+    public void writeValue(String key, String value, long time) {
+        BufferedWriter writer = writers.get(ThreadLocalRandom.current().nextInt(0, writers.size()));
+        synchronized (writer) {
+            underlyingStore.compute(key, (__, oldValue) -> {
+                final long oldTime = oldValue != null ? oldValue.getKey() : 0;
+                try {
+                    writer.write(key + ":" + value + ":" + time);
+                    writer.newLine();
+                    return time > oldTime ? new Pair<>(time, value) : oldValue;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return oldValue;
+                }
+            });
+        }
     }
 
     public String getValue(String key) {
